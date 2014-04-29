361f32db11add3f124d47924de03d7aed145889f
Stoneman1 <aleksi1578@msn.com>
2014-04-29 14:29:30 +0300

    Rakenteen muutos

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-var express     = require('express'),
-    swig        = require('swig'),
-    consolidate = require('consolidate'),
+var express = require('express')
+  , cons = require('consolidate')
+  , app = express();
 
-    app         = express();
+// assign the swig engine to .html files
+app.engine('html', cons.swig);
 
+// set .html as the default extension 
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
-
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.send(500, 'Something broke!');
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Alaston #uusikanava'
+  });
 });
 
-app.use('/static', express.static(__dirname + '/static'));
-
-app.get('/index', function(req, res){
-	res.send('Index');
+app.get('/images', function(req, res){
+  res.render('images', {
+    title: 'Kuvvee'
+  });
+});
+app.get('/login', function(req, res){
+  res.render('login', {
+    title: 'kirjaudu'
+  });
+});
+app.get('/logout', function(req, res){
+  res.render('logout', {
+    title: 'Kirjaudu ulos'
+  });
+});
+app.get('/addimg', function(req, res){
+  res.render('addimg', {
+    title: 'Lisää kuva'
+  });
+});
+app.get('/register', function(req, res){
+  res.render('register', {
+    title: 'Rekisteröidy'
+  });
 });
 
+app.listen(3000);
+console.log('Express server listening on port 3000');
 
-var server = app.listen(3000, function() {
-	console.log('Listening on port %d', server.address().port);
-});
